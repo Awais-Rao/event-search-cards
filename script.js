df234e34eab46450778da22f9de56e003761c7e4
Awais Rao <108420364+Awais-Rao@users.noreply.github.com>
2024-09-13 23:07:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// ========= Initialize Swiper =========
+
+var swiper = new Swiper(".card__images_swiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
